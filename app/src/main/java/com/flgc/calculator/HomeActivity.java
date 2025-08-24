81af18e77daec55665510c50ffbbbee411b0aa1c
Fábio Luís <ml@fabiotecnico1.biz>
2025-08-23 22:29:53 -0300

    Working out the logic for HomeActivity

--- app/src/main/java/com/flgc/calculator/HomeActivity.java
+++ app/src/main/java/com/flgc/calculator/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.flgc.calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,33 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btSum = findViewById(R.id.btSum);
+        btSum.setOnClickListener(this);
+
+        Button btMultiplication = findViewById(R.id.btMultiplication);
+        btMultiplication.setOnClickListener(this);
+
+        Button btDivision = findViewById(R.id.btDivision);
+        btDivision.setOnClickListener(this);
+
+        Button btSubtraction = findViewById(R.id.btSubtraction);
+        btSubtraction.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent screenCalculator = new Intent(HomeActivity.this, CalculatorActivity.class);
+        if (view.getId() == R.id.btSum){
+            screenCalculator.putExtra("operation", "makeTheSum");
+        } else if (view.getId() == R.id.btDivision) {
+            screenCalculator.putExtra("operation", "makeTheDivision");
+        } else if (view.getId() == R.id.btMultiplication){
+            screenCalculator.putExtra("operation", "makeTheMultiplication");
+        } else if (view.getId() == R.id.btSubtraction) {
+            screenCalculator.putExtra("operation", "makeTheSubtraction");
+        }
+
+        startActivity(screenCalculator);
     }
 }
